33e6712b3ba36ffe4e62527e5c09f866831b9b16
szymon <sz.rzad@gmail.com>
2023-07-26 18:07:56 +0200

    feat: Remove Galaxy Nexus as it is not receiving tests

--- buildSrc/src/main/kotlin/Devices.kt
+++ buildSrc/src/main/kotlin/Devices.kt
@@ -6,7 +6,7 @@ import org.gradle.kotlin.dsl.invoke
 
 // Device profiles taken from `$avdmanager list device` and tested locally for validity on 25/07/2023
 val devicesNames = listOf(
-    "Pixel 5", "Galaxy Nexus", "Nexus 10", "Nexus 4", "Nexus 5", "Nexus 5X", "Nexus 6", "Nexus 6P",
+    "Pixel 5", "Nexus 10", "Nexus 4", "Nexus 5", "Nexus 5X", "Nexus 6", "Nexus 6P",
     "Nexus 7", "Nexus 9", "Nexus One", "Nexus S", "Pixel", "Pixel 2", "Pixel 2 XL", "Pixel 3", "Pixel 3 XL",
     "Pixel 3a", "Pixel 3a XL", "Pixel 4", "Pixel 4 XL", "Pixel 4a", "Pixel C", "Pixel XL"
 )
